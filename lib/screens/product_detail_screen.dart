61eeab7729c9f80b417afb65a1617a97819a8dda
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-09-06 17:26:52 +0530

    189.adding navigation to the app

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -5,6 +5,8 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    //...
     return Scaffold(
       appBar: AppBar(
         title: Text("title"),
