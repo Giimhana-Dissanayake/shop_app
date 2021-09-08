b62f9f478b08520753e7a2c27d12246e51258ce6
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-09-08 16:33:15 +0530

    218. Adding a manage products

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 import 'providers/cart.dart';
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -36,7 +37,8 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.routeName: (ctx) => OrdersScreen()
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
