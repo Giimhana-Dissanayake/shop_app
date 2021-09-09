06b0f91b0fc2faef4e8ab961f95f9386a7ea5634
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-09-09 12:40:07 +0530

    251.utilizing optimistic updating

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -154,7 +154,20 @@ class Products with ChangeNotifier {
   }
 
   void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+    final url =
+        'https://coffee-application-70b98.firebaseio.com/products/$id.json';
+
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+
+    http.delete(url).then((response) {
+      if (response.statusCode < 400) {}
+      existingProduct = null;
+    }).catchError((_) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+    });
+    _items.removeAt(existingProductIndex);
     notifyListeners();
   }
 }
