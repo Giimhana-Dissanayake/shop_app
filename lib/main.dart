1e23554da8ef8d87f2d58e5f915ebb9bf099361d
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-09-11 15:51:27 +0530

    265.adding user signup

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import '../screens/product_detail_screen.dart';
 import '../screens/products_overview_screen.dart';
 import './providers/products.dart';
 import 'package:provider/provider.dart';
+import 'providers/auth.dart';
 import 'providers/cart.dart';
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
@@ -18,6 +19,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
